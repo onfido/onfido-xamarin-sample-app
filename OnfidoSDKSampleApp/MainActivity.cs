e3d0d299adfceb20bf9d5d7bf9014adfcb75e5ba
ricardo <derfreitas@gmail.com>
2017-10-10 10:20:38 -0700

    first commit
    ignored classes in UI, but not the nested package options

---
+++ OnfidoSDKSampleApp/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Com.Onfido.Android.Sdk.Capture;
+using Com.Onfido.Android.Sdk.Capture.UI.Options;
+using Com.Onfido.Api.Client.Data;
+
+namespace OnfidoSDKSampleApp
+{
+    [Activity(Label = "OnfidoSDKSampleApp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            FlowStep[] steps = { FlowStep.CaptureDocument, FlowStep.CaptureFace };
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            IOnfido client = OnfidoFactory.Create(this).Client;
+            Applicant applicant = Applicant.InvokeBuilder().WithFirstName("Dummy").WithLastName("Dummy").Build();
+            OnfidoConfig config = OnfidoConfig.InvokeBuilder()
+                .WithApplicant(applicant)
+                .WithToken("TOKEN")
+                .WithCustomFlow(steps)
+                .Build();
+            client.StartActivityForResult(this, 1, config);
+        }
+    }
+}
+
